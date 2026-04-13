b8c101d6d7c2d68614662310207f21f205321394
Solis Dynamics <meg-bjk@hotmail.com>
2026-04-13 18:11:58 +0300

    Update CollectionsExample.java

--- examples/java-util/CollectionsExample.java
+++ examples/java-util/CollectionsExample.java
@@ -1,1 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class CollectionsExample {
+    public static void main(String[] args) {
+        List<String> libraries = new ArrayList<>();
+        libraries.add("java.lang");
+        libraries.add("java.util");
+        libraries.add("java.io");
+
+        for (String library : libraries) {
+            System.out.println(library);
+        }
+    }
+}
